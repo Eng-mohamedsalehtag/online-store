dd9d55153fca691cff13da2528f0fd898dd40c27
Mohamed Saleh <mosalehtag74@icloud.com>
2026-05-02 23:49:40 +0300

    create product-details component

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { ProductsService } from 'src/app/services/products.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent {
+  productId: any;
+  product: any;
+  loading = true;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductsService,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.productId = params.get('id');
+      this.getProduct();
+    });
+  }
+
+  getProduct() {
+    this.productService.getProductById(this.productId).subscribe({
+      next: (res) => {
+        this.product = res;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.loading = false;
+      },
+    });
+  }
+  addToCart(product: any) {
+    let cart = JSON.parse(localStorage.getItem('cart') || '[]');
+    let existingProduct = cart.find((p: any) => p.id === product.id);
+    if (existingProduct) {
+      existingProduct.quantity += 1;
+    } else {
+      cart.push({ ...product, quantity: 1 });
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert('Product added to cart!');
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -9,4 +9,8 @@ export class ProductsService {
   getProducts() {
     return this.http.get('https://fakestoreapi.com/products');
   }
+
+  getProductById(id: number) {
+    return this.http.get(`https://fakestoreapi.com/products/${id}`);
+  }
 }
